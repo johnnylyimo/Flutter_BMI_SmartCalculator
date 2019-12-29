c162b8072031400c7ec7dde49245c6a70a6850e6
johnnylyimo <johnnylyimo@gmail.com>
2019-12-29 16:09:56 +0300

    Added ColorScheme onSurface

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class BMICalculator extends StatelessWidget {
     onPrimary: Colors.teal.shade700,
     secondary: Colors.white,
     surface: Colors.teal,
+    onSecondary: Colors.teal[900],
   );
 
   @override
