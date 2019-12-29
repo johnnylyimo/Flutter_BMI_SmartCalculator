85c974a02071ed5574792aee2158dd306cf1f1a3
johnnylyimo <johnnylyimo@gmail.com>
2019-12-29 15:36:44 +0300

    Added ThemeData accentColor

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class BMICalculator extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primaryColor: Colors.teal.shade500,
+        accentColor: Colors.teal.shade700,
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
