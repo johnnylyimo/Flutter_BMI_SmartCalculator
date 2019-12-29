58cef7c2abe05d1643d10abd4361df639a0121e1
johnnylyimo <johnnylyimo@gmail.com>
2019-12-29 15:22:56 +0300

    Renamed from MyApp to BMICalculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(BMICalculator());
 
-class MyApp extends StatelessWidget {
+class BMICalculator extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
