b873fc23d0ee1d18e1f1a60ea80f6beb79f2ac3f
johnnylyimo <johnnylyimo@gmail.com>
2019-12-30 14:58:26 +0300

    Added another Expanded widget to the Row children

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -21,6 +21,9 @@ class _HomePageState extends State<HomePage> {
               children: <Widget>[
                 Expanded(
                   child: null,
+                ),
+                Expanded(
+                  child: null,
                 )
               ],
             ),
