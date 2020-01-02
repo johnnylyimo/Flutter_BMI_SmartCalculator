ca2701356014f7d75d24f0af665c6a017f77b268
johnnylyimo <johnnylyimo@gmail.com>
2020-01-02 18:23:04 +0300

    Added cardChild with Column to the ReusableCard 5

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -165,6 +165,7 @@ class _HomePageState extends State<HomePage> {
                 Expanded(
                   child: ReusableCard(
                     color: inactiveCardColor,
+                    cardChild: Column(),
                   ),
                 )
               ],
