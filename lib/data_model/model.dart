f2d4ec7d98b274d980b0a7c32ec101ba0b73a92f
johnnylyimo <johnnylyimo@gmail.com>
2020-01-05 18:37:24 +0300

    Defined class method getInterpretation

--- lib/data_model/model.dart
+++ lib/data_model/model.dart
@@ -21,4 +21,14 @@ class BMICalculator {
       return 'UNDERWEIGHT';
     }
   }
+
+  String getInterpretation() {
+    if (_bmi >= 25) {
+      return 'You have a higher than normal body weight. Try to exercise more.';
+    } else if (_bmi > 18.5) {
+      return 'You have a normal body weight. Good job!';
+    } else {
+      return 'Your BMI result is quite low, you shoult eat more!';
+    }
+  }
 }
